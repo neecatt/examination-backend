4ab823d659fd953a6f78a8378fb6bb2c34477cd6
HorrorOTB <nicatabdullazade135@gmail.com>
2023-03-23 15:00:36 +0400

    configured cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
   app.enableCors({
     origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
+    preflightContinue: false,
   });
+
+  await app.listen(3000);
 }
 bootstrap();
