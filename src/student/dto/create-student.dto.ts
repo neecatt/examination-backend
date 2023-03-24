eabafd7c16ef285c2135e1edcbe8628901bdd134
HorrorOTB <nicatabdullazade135@gmail.com>
2023-03-24 14:51:56 +0400

    Added student CRUD

--- src/student/dto/create-student.dto.ts
+++ src/student/dto/create-student.dto.ts
@@ -1,1 +1,19 @@
-export class CreateStudentDto {}
+import { UniGroup } from '@prisma/client';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { CreateUnigroupDto } from 'src/unigroup/dto/create-unigroup.dto';
+
+export class CreateStudentDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  surname: string;
+
+  @IsNumber()
+  unigroupId: number;
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateTeacherDto } from 'src/teacher/dto/create-teacher.dto';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 
 @Injectable()
 export class StudentService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createStudentDto: CreateStudentDto) {
+    const { unigroupId, ...studentData } = createStudentDto;
+    // const unigroup = createStudentDto.unigroup;
+
+    try {
+      const student = await this.prisma.student.create({
+        data: {
+          ...studentData,
+          unigroup: {
+            connect: {
+              id: unigroupId,
+            },
+          },
+        },
+      });
+
+      return student;
+    } catch (error) {
+      throw error;
+    }
   }
 
   findAll() {
-    return `This action returns all student`;
+    return this.prisma.student.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} student`;
+    try {
+      return this.prisma.student.findUnique({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
   }
 
   update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
+    return this.prisma.student.update({
+      where: {
+        id,
+      },
+      data: {
+        ...updateStudentDto,
+      },
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} student`;
+    return this.prisma.student.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
