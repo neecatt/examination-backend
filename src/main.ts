bcb298a2c55a27f155cff450e85640ffcca67976
HorrorOTB <nicatabdullazade135@gmail.com>
2023-03-27 00:58:25 +0400

    added frontend_url environment variable

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
