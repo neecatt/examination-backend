8885cb67c7e3dfc467b0a14eeac1a46b3866e54c
HorrorOTB <nicatabdullazade135@gmail.com>
2023-03-28 00:45:37 +0400

    changes in create function of teacher service

--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -12,13 +12,8 @@ export class TeacherService {
   async create(createTeacherDto: CreateTeacherDto) {
     const salt = bcrypt.genSaltSync(10);
 
-    const { subjectIds, ...teacherData } = createTeacherDto;
-
     const hash = await bcrypt.hashSync(createTeacherDto.password, salt);
-    teacherData.password = hash;
-    const subjectIdArray = createTeacherDto.subjectIds.map((id) => {
-      return { Subject: { connect: { id } } };
-    });
+    createTeacherDto.password = hash;
 
     //throw exception if teacher email already exists
     const teacher = await this.prisma.teacher.findUnique({
@@ -31,11 +26,16 @@ export class TeacherService {
     }
 
     return await this.prisma.teacher.create({
-      data: {
-        Subjects: {
-          create: subjectIdArray,
-        },
-        ...teacherData,
+      data: createTeacherDto,
+      select: {
+        id: true,
+        name: true,
+        surname: true,
+        password: false,
+        email: true,
+        is_active: true,
+        Subjects: true,
+        Unigroups: true,
       },
     });
   }
@@ -64,13 +64,41 @@ export class TeacherService {
     return await this.findOneByEmail(teacher.email);
   }
 
-  update(id: number, updateTeacherDto: UpdateTeacherDto) {
-    return this.prisma.teacher.update({
-      where: {
-        id,
-      },
-      data: updateTeacherDto,
-    });
+  async update(id: number, updateTeacherDto: UpdateTeacherDto) {
+    try {
+      const { subjectIds, unigroupIds, ...teacherData } = updateTeacherDto;
+
+      const subjectIdArray = updateTeacherDto.subjectIds.map((id) => {
+        return { Subject: { connect: { id } } };
+      });
+
+      const unigroupIdArray = updateTeacherDto.unigroupIds.map((id) => {
+        return { UniGroup: { connect: { id } } };
+      });
+
+      return this.prisma.teacher.update({
+        where: {
+          id,
+        },
+        data: {
+          Subjects: { create: subjectIdArray },
+          Unigroups: { create: unigroupIdArray },
+          ...teacherData,
+        },
+        select: {
+          id: true,
+          name: true,
+          surname: true,
+          password: false,
+          email: true,
+          is_active: true,
+          Subjects: true,
+          Unigroups: true,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
   }
 
   async remove(id: number) {
