2a8f78507320174b3b9a2ac8e76468472af5af11
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-02 01:45:18 +0400

    added helper function for extracting docx

---
+++ src/question/helper/contentExtractor.ts
@@ -0,0 +1,35 @@
+function contentExtractor(file) {
+  const mammoth = require('mammoth');
+  return mammoth
+    .extractRawText({ path: file.path })
+    .then(function (result) {
+      const text = result.value; // The raw text
+      //split text by new line
+      const lines = text.split('\n');
+      //if line contains '' then remove it
+      for (let i = 0; i < lines.length + 1; i++) {
+        if (lines[i] == '' || lines[i] == ' ') {
+          lines.splice(i, 1);
+        }
+      }
+      //if last line is empty remove it
+      if (lines[lines.length - 1] == '' || lines[lines.length - 1] == ' ') {
+        lines.pop();
+      }
+      //Iterate over lines and if last line contains in one of them get this line
+      const length = lines.length;
+      const lastLine = lines[length - 1];
+      for (let i = 0; i < length - 2; i++) {
+        if (lines[i].includes(lastLine)) {
+        }
+      }
+      const messages = result.messages; // Any messages, such as warnings during conversion
+      // console.log(lines);
+      return lines;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export default contentExtractor;
