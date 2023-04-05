d291eed25559d695df676fe8fa745cc7a2e23bb4
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-05 17:51:58 +0400

    modified imports

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { CreateQuizDto } from './dto/create-quiz.dto';
 import { UpdateQuizDto } from './dto/update-quiz.dto';
